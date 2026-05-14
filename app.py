b04bab29fc4bcad54eac1ed8059ce1fbcd1e4031
Danilo Augusto Salvego <Danilosalvego@gmail.com>
2026-05-14 16:29:12 -0300

    Add studies endpoint

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     return {"message": "Backend is running."}
+
+
+@app.get("/studies")
+def studies():
+    return {
+        "current_focus": [
+            "Python",
+            "FastAPI",
+            "Git",
+            "Backend Development"
+        ]
+    }
